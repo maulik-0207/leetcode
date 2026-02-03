64c33614c695e10225cda7075f77e97cd005a961
Maulik <139668060+maulik-4200@users.noreply.github.com>
2026-02-03 09:25:57 +0530

    Solved 5

---
+++ 5-longest-palindromic-substring.py
@@ -0,0 +1,60 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Old approach
+        # length = len(s)
+        # if length == 1: return s
+        # longest_palindrome_start = longest_palindrome_ends = longest_palindrome_len = -1
+        # for i in range(length):
+        #     for j in range(i, length):
+        #         if s[i:j + 1] == (s[i:j+1])[::-1]:
+        #             if (j+1)-i > longest_palindrome_len:
+        #                 longest_palindrome_len = (j+1)-i
+        #                 longest_palindrome_start = i
+        #                 longest_palindrome_ends = j
+        # if longest_palindrome_len:
+        #     return s[longest_palindrome_start:longest_palindrome_ends+1]
+        # else:
+        #     return None
+        
+        if not s:
+            return ""
+        start = end = 0
+        
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return right - left - 1
+        
+        for i in range(len(s)):
+            len1 = expand(i,i)
+            len2 = expand(i,i+1)
+            
+            max_len = max(len1, len2)
+
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start:end + 1]
+                
+
+if __name__ == '__main__':
+    obj = Solution()
+    ans = obj.longestPalindrome("babad")
+    ans1 = obj.longestPalindrome("cbbd")
+    ans2 = obj.longestPalindrome("a")
+    ans3 = obj.longestPalindrome("ac")
+    ans4 = obj.longestPalindrome("busislnescsicxpvvysuqgcudefrfjbwwjcchtgqyajdfwvkypfwshnihjdztgmyuuljxgvhdiwphrweyfkbnjgerkmifbirubhseuhrugwrabnjafnbdfjnufdstjbkuwtnpflffaqmjbhssjlnqftgjiglvvequhapasarlkcvbmkwnkuvwktbgfoaxteprobdwswcdyddyvrehvmxrrjiiidatidlpihkbmmruysmhhsncmfdanafdrfpdtfgkglcqpwrrtvacuicohspkounojuziittugpqjyhhkwfnflozbispehrtrnizowrlzcuollagxwtznjwzcumvedjwokueuqktvvouwnsmpxqvvpuwprezrbobrpnwaccwljchdguubjulyilzvmandjjleitweybqkjttschrjjlebnmponvlktzzcdtuybugggcqffkcffpamauvxfbonjrobgpvlyzveiwemmtdvbjciaytvesnocnjrwodtcokgcuoiicxapmrzpkfphjniuvzjrhbnqndfshoduejyktebgdabidxlkstepuwvtrtgbxaeheylicvhrxddijshcvdadxzsccmainyfpfdhqdanfccqkzlmhsfilvoybqojlvbcixjzqpbngdvesuokbxhkomsiqfyukvspqthlzxdnlwthrgaxhtpjzhrugqbfokrdcyurivmzgtynoqfjbafboselxnfupnpqlryvlcxeksirvufepfwczosrrjpudbwqxwldgjyfjhzlzcojxyqjyxxiqvfhjdwtgoqbyeocffnyxhyyiqspnvrpxmrtcnviukrjvpavervvztoxajriuvxqveqsrttjqepvvahywuzwtmgyrzduxfqspeipimyoxmkadrvrdyefekjxcmsmzmtbugyckcbjsrymszftjyllfmoeoylzeahnrxlxpnlvlvzltwnmldi")
+    ans5 = obj.longestPalindrome("jaliztdispcppzgzjxnjxwbhhtbjrijyibqwrhwuscmokylygielwssuyretqnoiglvsltmhetvdoliwibrnwmdtauczcswuqxxokaykslfzgxovphdptgtrbbozdkdgawcegemkumgbyqzjrzurkdaibfwwvcxfcstvixisrcfxvnlzizlbnacxssetlsxrvcaqvzmbnzdfmtskmxmjblzgpdsjvhqhrihiajvwxbmjsncjhmilercbdbzyncrnlyrxrefaeuttkscfttqnedzvqisclbremuxmngrpgqjqkijpizkixkrgaarpknivrrirbkeddkulvlfuetbdnugzodbfufqhrpkyufhrhnnnzsenkvqsuhlbaimniusuxsnmavqbilzgsfxjykrxdkkpnneikwlucdghnikojythrpgwyzoqgraycavrivsbfuaonssmryhcykooivrxmeeowllsfeyxrznvkdpobohpzolnpbxjjxbpnlozphobopdkvnzrxyefsllwoeemxrviookychyrmssnoaufbsvirvacyargqozywgprhtyjokinhgdculwkiennpkkdxrkyjxfsgzlibqvamnsxusuinmiablhusqvknesznnnhrhfuykprhqfufbdozgundbteuflvlukddekbrirrvinkpraagrkxikzipjikqjq")
+    print(ans)
+    print(ans1)
+    print(ans2)
+    print(ans3)
+    print(ans4)
+    print(ans5)
+    
